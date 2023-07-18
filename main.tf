fa73e7b17b5a5a7b046508df36377f8e6a63547f
Kevin Woo <woowkk@sph.com.sg>
2023-07-18 00:38:50 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -198,7 +198,6 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-
 module "s3-bucket" {
   source              = "cloudposse/s3-bucket/aws"
   version             = "3.1.0"
